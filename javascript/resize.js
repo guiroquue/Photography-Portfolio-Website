c2ecab095f136108d5c1069f1b602f989ed5a2f3
Gui <70318174+GuiRoque04@users.noreply.github.com>
2023-12-13 21:17:55 +0000

    updated website design

---
+++ javascript/resize.js
@@ -0,0 +1,51 @@
+window.addEventListener('resize', function() {
+    const windowWidth = window.innerWidth;
+
+    const headerLinks = document.getElementById('headerLinks');
+
+    if (windowWidth <= 1200) {
+        headerLinks.style.display = 'none';
+        hamburgerMenu.style.display = 'block';
+
+    } else {
+        headerLinks.style.display = 'block';
+        headerLinks.style.position = 'static';
+        hamburgerMenu.style.display = 'none';
+
+    }
+});
+
+function toggleMenu() {
+    const headerLinks = document.getElementById('headerLinks');
+    const overlay = document.getElementById('overlay');
+
+    const body = document.body;
+
+    if (headerLinks.style.display === 'none' || headerLinks.style.display === '') {
+        headerLinks.style.display = 'block';
+        headerLinks.style.position = 'absolute';
+        headerLinks.style.top = '50%';
+        headerLinks.style.left = '50%';
+        headerLinks.style.transform = 'translate(-50%, -50%)';
+
+        body.style.overflow = 'hidden';
+
+        overlay.style.display = 'block';
+        overlay.style.pointerEvents = 'auto';
+    } else {
+        headerLinks.style.display = 'block';
+        headerLinks.style.display = 'none';
+
+        headerLinks.style.top = '0%';
+        headerLinks.style.left = '0%';
+        headerLinks.style.transform = 'translate(0%, 0%)';
+
+        body.style.overflow = 'auto';
+
+        overlay.style.display = 'none';
+        overlay.style.pointerEvents = 'none';
+    }
+}
+
+
+window.dispatchEvent(new Event('resize'));
