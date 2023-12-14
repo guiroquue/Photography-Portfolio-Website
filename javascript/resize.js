b7df8e6a688d27b5a0d4f1aac592a47eb977ee36
Gui <70318174+GuiRoque04@users.noreply.github.com>
2023-12-14 03:26:06 +0000

    Design update

--- javascript/resize.js
+++ javascript/resize.js
@@ -2,18 +2,45 @@ window.addEventListener('resize', function() {
     const windowWidth = window.innerWidth;
 
     const headerLinks = document.getElementById('headerLinks');
+    const disapearingImages = document.getElementById("gone");
 
     if (windowWidth <= 1200) {
         headerLinks.style.display = 'none';
         hamburgerMenu.style.display = 'block';
 
     } else {
+        disapearingImages.style.display = "block";
         headerLinks.style.display = 'block';
         headerLinks.style.position = 'static';
         hamburgerMenu.style.display = 'none';
 
     }
 });
+
+// Select all images with a specific class
+const images = document.querySelectorAll('.responsive-image');
+
+function toggleImages() {
+    // Check window width
+    const windowWidth = window.innerWidth;
+
+    // Define the threshold width to hide images
+    const thresholdWidth = 1200; // For example, adjust this as needed
+
+    // Loop through the images and toggle their visibility based on window width
+    images.forEach(image => {
+        if (windowWidth <= thresholdWidth) {
+            image.style.display = 'none'; // Hide images
+        } else {
+            image.style.display = 'block'; // Show images
+        }
+    });
+}
+
+// Call toggleImages() initially and add event listener for window resize
+toggleImages(); // To set visibility initially based on window width
+window.addEventListener('resize', toggleImages); // To handle changes on window resize
+
 
 function toggleMenu() {
     const headerLinks = document.getElementById('headerLinks');
