1f697d60289db7fc3ecd6c1bc19adf781f400339
Gui <70318174+GuiRoque04@users.noreply.github.com>
2024-04-24 11:12:11 +0000

    faq filters, mobile fix

--- javascript/generateFaqItems.js
+++ javascript/generateFaqItems.js
@@ -1,141 +1,161 @@
-// Load the JSON file containing FAQ data
-fetch('./json/faqQuestionsAnswers.json')
-.then(response => response.json())
-    .then(data => {
-        // Initialize the FAQ items container
-        const faqItemsContainer = document.getElementById('faq-items-container');
-        
-        // Function to generate FAQ items based on the selected category
-        function displayFAQItems(category) {
-            // Clear the current FAQ items
-            faqItemsContainer.innerHTML = '';
+// Function to fetch JSON data and initialize the FAQ display
+function fetchFAQDataAndInitialize() {
+    // Fetch JSON data
+    fetch('./json/faqQuestionsAnswers.json')
+        .then(response => response.json())
+        .then(data => {
+            // Initialize filter tabs and display all FAQ items initially
+            setUpFilterTabs(data);
+            displayFAQItems('All', data);
+        })
+        .catch(error => {
+            console.error('Error loading FAQ data:', error);
+            // Add error handling as needed (e.g., display an error message to the user)
+        });
+}
 
-            // Filter the data based on the selected category
-            const filteredData = category === 'All' ? data : data.filter(item => item.category === category);
+// Function to display FAQ items based on the selected category
+function displayFAQItems(category, data) {
+    const faqItemsContainer = document.getElementById('faq-items-container');
+    faqItemsContainer.innerHTML = ''; // Clear current FAQ items
 
-            // Track the displayed categories
-            const displayedCategories = new Set();
+    // Filter data based on the selected category
+    const filteredData = category === 'All' ? data : data.filter(item => item.category === category);
 
-            // Generate the HTML for each FAQ item
-            filteredData.forEach(item => {
-                // Check if the current category has been displayed
-                if (!displayedCategories.has(item.category)) {
-                    // Create an h2 element for the category
-                    const categoryHeading = document.createElement('h2');
-                    categoryHeading.innerText = item.category;
-                    faqItemsContainer.appendChild(categoryHeading);
+    // Set to track displayed categories (to avoid duplicate category headers)
+    const displayedCategories = new Set();
 
-                    // Add the category to the set of displayed categories
-                    displayedCategories.add(item.category);
-                }
+    // Generate HTML for each FAQ item
+    filteredData.forEach(item => {
+        // Check if category header needs to be displayed
+        if (!displayedCategories.has(item.category)) {
+            // Create category header and add to container
+            const categoryHeader = document.createElement('h2');
+            categoryHeader.textContent = item.category;
+            faqItemsContainer.appendChild(categoryHeader);
 
-                // Create a div element for the FAQ item
-                const faqItem = document.createElement('div');
-                faqItem.classList.add('faq-item');
-
-                // Create a div element for the FAQ info
-                const faqInfo = document.createElement('div');
-                faqInfo.classList.add('faq-info');
-                
-                // Create an h3 element for the question
-                const question = document.createElement('h3');
-                question.classList.add('question');
-                question.innerHTML = item.question;
-                
-                // Create an img element for the arrow icon
-                const arrowIcon = document.createElement('img');
-                arrowIcon.src = './assets/icons/arrowDownIcon.svg';
-                arrowIcon.alt = 'Image Not Found';
-                arrowIcon.style.width = '30px';
-                arrowIcon.style.height = '30px';
-                arrowIcon.style.transition = 'transform 0.3s'; // Smooth transition for rotation
-                
-                // Append the arrow icon after the question
-                faqInfo.appendChild(question);
-                faqInfo.appendChild(arrowIcon);
-                
-                // Create a p element for the answer and hide it initially
-                const answer = document.createElement('p');
-                answer.classList.add('answer');
-                answer.innerHTML = item.answer;
-                answer.style.display = 'none'; // Hide the answer initially
-
-                // Append the FAQ info and answer to the FAQ item
-                faqItem.appendChild(faqInfo);
-                faqItem.appendChild(answer);
-
-                // Add click event listener to toggle the answer visibility and rotate the arrow icon
-                faqItem.addEventListener('click', () => {
-                    // Toggle the visibility of the answer
-                    answer.style.display = answer.style.display === 'none' ? 'block' : 'none';
-                    
-                    // Rotate the arrow icon 90 degrees if answer is visible, otherwise reset rotation
-                    if (answer.style.display === 'block') {
-                        arrowIcon.style.transform = 'rotate(180deg)';
-                    } else {
-                        arrowIcon.style.transform = 'rotate(0deg)';
-                    }
-                });
-
-                // Append the FAQ item to the FAQ items container
-                faqItemsContainer.appendChild(faqItem);
-            });
+            // Mark category as displayed
+            displayedCategories.add(item.category);
         }
 
+        // Create FAQ item and add to container
+        const faqItem = createFAQItem(item);
+        faqItemsContainer.appendChild(faqItem);
+    });
+}
 
-        // Function to handle filter tab clicks
-        function handleFilterClick(event) {
-            // Remove the 'active' class from all tabs
-            const filterTabs = document.querySelectorAll('.filter-tab-item');
-            filterTabs.forEach(tab => tab.classList.remove('active'));
+// Function to create a single FAQ item
+function createFAQItem(item) {
+    const faqItem = document.createElement('div');
+    faqItem.classList.add('faq-item');
 
-            // Add the 'active' class to the clicked tab
-            event.currentTarget.classList.add('active');
+    // Create FAQ question element and append to FAQ item
+    const questionElement = createQuestionElement(item.question);
+    faqItem.appendChild(questionElement);
 
-            // Get the category from the clicked tab
-            const category = event.currentTarget.innerText;
+    // Create FAQ answer element and append to FAQ item
+    const answerElement = createAnswerElement(item.answer);
+    faqItem.appendChild(answerElement);
 
-            // Apply fade-out effect to the FAQ items container
-            faqItemsContainer.classList.add('fade-out');
+    // Add click event listener to FAQ item
+    faqItem.addEventListener('click', () => {
+        // Toggle answer visibility
+        const isAnswerVisible = answerElement.style.display === 'block';
+        answerElement.style.display = isAnswerVisible ? 'none' : 'block';
 
-            // After a short delay, display the FAQ items and apply fade-in effect
-            setTimeout(() => {
-                // Display the FAQ items for the selected category
-                displayFAQItems(category);
+        // Toggle arrow rotation
+        const arrowIcon = questionElement.querySelector('.arrow-icon');
+        arrowIcon.style.transform = isAnswerVisible ? 'rotate(0deg)' : 'rotate(180deg)';
+    });
 
-                // Remove the fade-out class and add fade-in effect
-                faqItemsContainer.classList.remove('fade-out');
-                faqItemsContainer.classList.add('fade-in');
+    return faqItem;
+}
 
-                // Remove the fade-in class after the animation ends
-                setTimeout(() => {
-                    faqItemsContainer.classList.remove('fade-in');
-                }, 50); // Adjust this time based on your preference for the fade-in duration
-            }, 50); // Adjust this time based on your preference for the fade-out duration
+// Function to create the FAQ question element
+function createQuestionElement(question) {
+    const questionContainer = document.createElement('div');
+    questionContainer.classList.add('faq-question');
+
+    const questionText = document.createElement('h3');
+    questionText.textContent = question;
+
+    // Arrow icon for toggling answer visibility
+    const arrowIcon = document.createElement('img');
+    arrowIcon.src = './assets/icons/arrowDownIcon.svg';
+    arrowIcon.alt = 'Arrow Icon';
+    arrowIcon.classList.add('arrow-icon');
+
+    // Append question text and arrow icon to the question container
+    questionContainer.appendChild(questionText);
+    questionContainer.appendChild(arrowIcon);
+
+    return questionContainer;
+}
+
+// Function to create the FAQ answer element
+function createAnswerElement(answer) {
+    const answerElement = document.createElement('p');
+    answerElement.classList.add('faq-answer');
+    answerElement.textContent = answer;
+
+    // Initially hide the answer
+    answerElement.style.display = 'none';
+
+    return answerElement;
+}
+
+// Function to handle filter tab clicks
+function handleFilterClick(event, data) {
+    // Remove the 'active' class from all tabs
+    const filterTabs = document.querySelectorAll('.filter-tab-item');
+    filterTabs.forEach(tab => tab.classList.remove('active'));
+
+    // Add the 'active' class to the clicked tab
+    event.currentTarget.classList.add('active');
+
+    // Get the selected category
+    const category = event.currentTarget.textContent.trim();
+
+    // Reference the FAQ items container
+    const faqItemsContainer = document.getElementById('faq-items-container');
+
+    // Apply the fade-out effect by adding the 'fade-out' class
+    faqItemsContainer.classList.add('fade-out');
+
+    // After a delay to allow the fade-out effect to complete, display the FAQ items and apply the fade-in effect
+    setTimeout(() => {
+        // Display the FAQ items for the selected category
+        displayFAQItems(category, data);
+
+        // Remove the fade-out class and add the fade-in effect
+        faqItemsContainer.classList.remove('fade-out');
+        faqItemsContainer.classList.add('fade-in');
+
+        // Remove the fade-in class after a short delay (equal to the CSS transition duration)
+        setTimeout(() => {
+            faqItemsContainer.classList.remove('fade-in');
+        }, 50); // Adjust this time based on the CSS transition duration
+    }, 50); // Adjust this time based on the CSS transition duration
+}
+
+
+// Function to set up filter tabs with click event listeners
+function setUpFilterTabs(data) {
+    const filterTabsContainer = document.getElementById('filter-tabs-container');
+    
+    // Add click event listeners to each filter tab
+    const filterTabs = filterTabsContainer.querySelectorAll('.filter-tab-item');
+    filterTabs.forEach(tab => {
+        tab.addEventListener('click', (event) => handleFilterClick(event, data));
+    });
+
+    // Set the 'All' tab as active initially
+    filterTabs.forEach(tab => {
+        if (tab.textContent.trim().toLowerCase() === 'all') {
+            tab.classList.add('active');
         }
+    });
+}
 
-        // Set up filter tabs
-        function setUpFilterTabs() {
-            const filterTabsContainer = document.getElementById('filter-tabs-container');
-            const filterTabs = filterTabsContainer.querySelectorAll('.filter-tab-item');
-
-            // Add click event listeners to each filter tab
-            filterTabs.forEach(tab => {
-                tab.addEventListener('click', handleFilterClick);
-            });
-
-            // Set the "All" tab as active initially
-            filterTabs.forEach(tab => {
-                if (tab.textContent.trim().toLowerCase() === 'all') {
-                    tab.classList.add('active');
-                }
-            });
-        }
-
-        // Set up the filter tabs
-        setUpFilterTabs();
-
-        // Set the initial state: "All" filter selected
-        displayFAQItems('All');
-    })
-    .catch(error => console.error('Error loading FAQ data:', error));
+// Initialize the FAQ display
+fetchFAQDataAndInitialize();
